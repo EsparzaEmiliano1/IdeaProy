ca48a864309640f39f57d81de0d9ee09024dd462
Emiliano Esparza <esparzaramirezemiliano@gmail.com>
2024-11-24 07:11:18 -0600

    first commit

---
+++ WinFormsAppIdeaProy/Form1.Designer.cs
@@ -0,0 +1,437 @@
+﻿namespace WinFormsAppIdeaProy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelPVCafe = new Label();
+            pictureBoxProd1 = new PictureBox();
+            pictureBoxProd2 = new PictureBox();
+            pictureBoxProd3 = new PictureBox();
+            pictureBoxProd4 = new PictureBox();
+            pictureBoxProd5 = new PictureBox();
+            pictureBoxProd6 = new PictureBox();
+            pictureBoxProd7 = new PictureBox();
+            pictureBoxProd8 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            labelCargarImg = new Label();
+            comboBox1 = new ComboBox();
+            buttonCargImg = new Button();
+            textBoxNomProd = new TextBox();
+            labelNomProd1 = new Label();
+            labelNomProd2 = new Label();
+            labelNomProd3 = new Label();
+            labelNomProd4 = new Label();
+            labelNomProd5 = new Label();
+            labelNomProd6 = new Label();
+            labelNomProd7 = new Label();
+            labelNomProd8 = new Label();
+            dataGridViewTicket = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTicket).BeginInit();
+            SuspendLayout();
+            // 
+            // labelPVCafe
+            // 
+            labelPVCafe.AutoSize = true;
+            labelPVCafe.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            labelPVCafe.Location = new Point(247, 9);
+            labelPVCafe.Name = "labelPVCafe";
+            labelPVCafe.Size = new Size(260, 22);
+            labelPVCafe.TabIndex = 0;
+            labelPVCafe.Text = "Punto de venta de un café";
+            // 
+            // pictureBoxProd1
+            // 
+            pictureBoxProd1.BackColor = SystemColors.ControlLight;
+            pictureBoxProd1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd1.Location = new Point(30, 131);
+            pictureBoxProd1.Name = "pictureBoxProd1";
+            pictureBoxProd1.Size = new Size(137, 57);
+            pictureBoxProd1.TabIndex = 1;
+            pictureBoxProd1.TabStop = false;
+            // 
+            // pictureBoxProd2
+            // 
+            pictureBoxProd2.BackColor = SystemColors.ControlLight;
+            pictureBoxProd2.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd2.Location = new Point(225, 131);
+            pictureBoxProd2.Name = "pictureBoxProd2";
+            pictureBoxProd2.Size = new Size(137, 57);
+            pictureBoxProd2.TabIndex = 2;
+            pictureBoxProd2.TabStop = false;
+            // 
+            // pictureBoxProd3
+            // 
+            pictureBoxProd3.BackColor = SystemColors.ControlLight;
+            pictureBoxProd3.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd3.Location = new Point(30, 252);
+            pictureBoxProd3.Name = "pictureBoxProd3";
+            pictureBoxProd3.Size = new Size(137, 57);
+            pictureBoxProd3.TabIndex = 3;
+            pictureBoxProd3.TabStop = false;
+            // 
+            // pictureBoxProd4
+            // 
+            pictureBoxProd4.BackColor = SystemColors.ControlLight;
+            pictureBoxProd4.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd4.Location = new Point(225, 252);
+            pictureBoxProd4.Name = "pictureBoxProd4";
+            pictureBoxProd4.Size = new Size(137, 57);
+            pictureBoxProd4.TabIndex = 4;
+            pictureBoxProd4.TabStop = false;
+            // 
+            // pictureBoxProd5
+            // 
+            pictureBoxProd5.BackColor = SystemColors.ControlLight;
+            pictureBoxProd5.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd5.Location = new Point(30, 369);
+            pictureBoxProd5.Name = "pictureBoxProd5";
+            pictureBoxProd5.Size = new Size(137, 57);
+            pictureBoxProd5.TabIndex = 5;
+            pictureBoxProd5.TabStop = false;
+            // 
+            // pictureBoxProd6
+            // 
+            pictureBoxProd6.BackColor = SystemColors.ControlLight;
+            pictureBoxProd6.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd6.Location = new Point(225, 369);
+            pictureBoxProd6.Name = "pictureBoxProd6";
+            pictureBoxProd6.Size = new Size(137, 57);
+            pictureBoxProd6.TabIndex = 6;
+            pictureBoxProd6.TabStop = false;
+            // 
+            // pictureBoxProd7
+            // 
+            pictureBoxProd7.BackColor = SystemColors.ControlLight;
+            pictureBoxProd7.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd7.Location = new Point(30, 491);
+            pictureBoxProd7.Name = "pictureBoxProd7";
+            pictureBoxProd7.Size = new Size(137, 57);
+            pictureBoxProd7.TabIndex = 7;
+            pictureBoxProd7.TabStop = false;
+            // 
+            // pictureBoxProd8
+            // 
+            pictureBoxProd8.BackColor = SystemColors.ControlLight;
+            pictureBoxProd8.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxProd8.Location = new Point(225, 491);
+            pictureBoxProd8.Name = "pictureBoxProd8";
+            pictureBoxProd8.Size = new Size(137, 57);
+            pictureBoxProd8.TabIndex = 8;
+            pictureBoxProd8.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(30, 193);
+            button1.Name = "button1";
+            button1.Size = new Size(137, 23);
+            button1.TabIndex = 9;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(225, 193);
+            button2.Name = "button2";
+            button2.Size = new Size(137, 23);
+            button2.TabIndex = 10;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(30, 315);
+            button3.Name = "button3";
+            button3.Size = new Size(137, 23);
+            button3.TabIndex = 11;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(225, 315);
+            button4.Name = "button4";
+            button4.Size = new Size(137, 23);
+            button4.TabIndex = 12;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(30, 432);
+            button5.Name = "button5";
+            button5.Size = new Size(137, 23);
+            button5.TabIndex = 13;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(225, 432);
+            button6.Name = "button6";
+            button6.Size = new Size(137, 23);
+            button6.TabIndex = 14;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(225, 554);
+            button7.Name = "button7";
+            button7.Size = new Size(137, 23);
+            button7.TabIndex = 16;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(30, 554);
+            button8.Name = "button8";
+            button8.Size = new Size(137, 23);
+            button8.TabIndex = 15;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // labelCargarImg
+            // 
+            labelCargarImg.AutoSize = true;
+            labelCargarImg.Location = new Point(36, 55);
+            labelCargarImg.Name = "labelCargarImg";
+            labelCargarImg.Size = new Size(94, 15);
+            labelCargarImg.TabIndex = 17;
+            labelCargarImg.Text = "Añadir Producto";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(146, 81);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(86, 23);
+            comboBox1.TabIndex = 18;
+            // 
+            // buttonCargImg
+            // 
+            buttonCargImg.Location = new Point(247, 52);
+            buttonCargImg.Name = "buttonCargImg";
+            buttonCargImg.Size = new Size(95, 23);
+            buttonCargImg.TabIndex = 19;
+            buttonCargImg.Text = "Añadir";
+            buttonCargImg.UseVisualStyleBackColor = true;
+            buttonCargImg.Click += button9_Click;
+            // 
+            // textBoxNomProd
+            // 
+            textBoxNomProd.Location = new Point(146, 52);
+            textBoxNomProd.Name = "textBoxNomProd";
+            textBoxNomProd.PlaceholderText = "Nom Prod";
+            textBoxNomProd.Size = new Size(86, 23);
+            textBoxNomProd.TabIndex = 20;
+            // 
+            // labelNomProd1
+            // 
+            labelNomProd1.AutoSize = true;
+            labelNomProd1.Location = new Point(30, 107);
+            labelNomProd1.Name = "labelNomProd1";
+            labelNomProd1.Size = new Size(122, 15);
+            labelNomProd1.TabIndex = 21;
+            labelNomProd1.Text = "Producto indefinido 1";
+            // 
+            // labelNomProd2
+            // 
+            labelNomProd2.AutoSize = true;
+            labelNomProd2.Location = new Point(225, 107);
+            labelNomProd2.Name = "labelNomProd2";
+            labelNomProd2.Size = new Size(122, 15);
+            labelNomProd2.TabIndex = 22;
+            labelNomProd2.Text = "Producto indefinido 2";
+            // 
+            // labelNomProd3
+            // 
+            labelNomProd3.AutoSize = true;
+            labelNomProd3.Location = new Point(30, 234);
+            labelNomProd3.Name = "labelNomProd3";
+            labelNomProd3.Size = new Size(122, 15);
+            labelNomProd3.TabIndex = 23;
+            labelNomProd3.Text = "Producto indefinido 3";
+            // 
+            // labelNomProd4
+            // 
+            labelNomProd4.AutoSize = true;
+            labelNomProd4.Location = new Point(225, 234);
+            labelNomProd4.Name = "labelNomProd4";
+            labelNomProd4.Size = new Size(122, 15);
+            labelNomProd4.TabIndex = 24;
+            labelNomProd4.Text = "Producto indefinido 4";
+            // 
+            // labelNomProd5
+            // 
+            labelNomProd5.AutoSize = true;
+            labelNomProd5.Location = new Point(30, 351);
+            labelNomProd5.Name = "labelNomProd5";
+            labelNomProd5.Size = new Size(122, 15);
+            labelNomProd5.TabIndex = 25;
+            labelNomProd5.Text = "Producto indefinido 5";
+            // 
+            // labelNomProd6
+            // 
+            labelNomProd6.AutoSize = true;
+            labelNomProd6.Location = new Point(225, 351);
+            labelNomProd6.Name = "labelNomProd6";
+            labelNomProd6.Size = new Size(122, 15);
+            labelNomProd6.TabIndex = 26;
+            labelNomProd6.Text = "Producto indefinido 6";
+            // 
+            // labelNomProd7
+            // 
+            labelNomProd7.AutoSize = true;
+            labelNomProd7.Location = new Point(30, 473);
+            labelNomProd7.Name = "labelNomProd7";
+            labelNomProd7.Size = new Size(122, 15);
+            labelNomProd7.TabIndex = 27;
+            labelNomProd7.Text = "Producto indefinido 7";
+            // 
+            // labelNomProd8
+            // 
+            labelNomProd8.AutoSize = true;
+            labelNomProd8.Location = new Point(225, 473);
+            labelNomProd8.Name = "labelNomProd8";
+            labelNomProd8.RightToLeft = RightToLeft.No;
+            labelNomProd8.Size = new Size(122, 15);
+            labelNomProd8.TabIndex = 28;
+            labelNomProd8.Text = "Producto indefinido 8";
+            // 
+            // dataGridViewTicket
+            // 
+            dataGridViewTicket.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewTicket.Location = new Point(424, 131);
+            dataGridViewTicket.Name = "dataGridViewTicket";
+            dataGridViewTicket.RowTemplate.Height = 25;
+            dataGridViewTicket.Size = new Size(344, 446);
+            dataGridViewTicket.TabIndex = 29;
+            dataGridViewTicket.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(801, 642);
+            Controls.Add(dataGridViewTicket);
+            Controls.Add(labelNomProd8);
+            Controls.Add(labelNomProd7);
+            Controls.Add(labelNomProd6);
+            Controls.Add(labelNomProd5);
+            Controls.Add(labelNomProd4);
+            Controls.Add(labelNomProd3);
+            Controls.Add(labelNomProd2);
+            Controls.Add(labelNomProd1);
+            Controls.Add(textBoxNomProd);
+            Controls.Add(buttonCargImg);
+            Controls.Add(comboBox1);
+            Controls.Add(labelCargarImg);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBoxProd8);
+            Controls.Add(pictureBoxProd7);
+            Controls.Add(pictureBoxProd6);
+            Controls.Add(pictureBoxProd5);
+            Controls.Add(pictureBoxProd4);
+            Controls.Add(pictureBoxProd3);
+            Controls.Add(pictureBoxProd2);
+            Controls.Add(pictureBoxProd1);
+            Controls.Add(labelPVCafe);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxProd8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTicket).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelPVCafe;
+        private PictureBox pictureBoxProd1;
+        private PictureBox pictureBoxProd2;
+        private PictureBox pictureBoxProd3;
+        private PictureBox pictureBoxProd4;
+        private PictureBox pictureBoxProd5;
+        private PictureBox pictureBoxProd6;
+        private PictureBox pictureBoxProd7;
+        private PictureBox pictureBoxProd8;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Label labelCargarImg;
+        private ComboBox comboBox1;
+        private Button buttonCargImg;
+        private TextBox textBoxNomProd;
+        private Label labelNomProd1;
+        private Label labelNomProd2;
+        private Label labelNomProd3;
+        private Label labelNomProd4;
+        private Label labelNomProd5;
+        private Label labelNomProd6;
+        private Label labelNomProd7;
+        private Label labelNomProd8;
+        private DataGridView dataGridViewTicket;
+    }
+}
---
+++ WinFormsAppIdeaProy/Form1.cs
@@ -0,0 +1,136 @@
+using System.Data;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormsAppIdeaProy
+{
+    public partial class Form1 : Form
+    {
+        DataTable ticket;
+        public Form1()
+        {
+            InitializeComponent();
+            Iniciar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Producto 1");
+            comboBox1.Items.Add("Producto 2");
+            comboBox1.Items.Add("Producto 3");
+            comboBox1.Items.Add("Producto 4");
+            comboBox1.Items.Add("Producto 5");
+            comboBox1.Items.Add("Producto 6");
+            comboBox1.Items.Add("Producto 7");
+            comboBox1.Items.Add("Producto 8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrirImagen = new OpenFileDialog();
+
+            if (abrirImagen.ShowDialog() == DialogResult.OK)
+            {
+                if (comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Por favor, selecciona un PictureBox antes de cargar la imagen.");  /// el problema esta en que se abre primero el abrirImagen
+                    return;                                                                             /// y despues manda ese error prob sea mejor primero este 
+                }                                                                                       /// y luego abrir la imagen <hay que cambiarlo>
+                string selectedBox = comboBox1.SelectedItem.ToString();
+                PictureBox selectedPictureBox = null;
+
+                switch (selectedBox)
+                {
+                    case "Producto 1":
+                        selectedPictureBox = pictureBoxProd1;
+                        break;
+                    case "Producto 2":
+                        selectedPictureBox = pictureBoxProd2;
+                        break;
+                    case "Producto 3":
+                        selectedPictureBox = pictureBoxProd3;
+                        break;
+                    case "Producto 4":
+                        selectedPictureBox = pictureBoxProd4;
+                        break;
+                    case "Producto 5":
+                        selectedPictureBox = pictureBoxProd5;
+                        break;
+                    case "Producto 6":
+                        selectedPictureBox = pictureBoxProd6;
+                        break;
+                    case "Producto 7":
+                        selectedPictureBox = pictureBoxProd7;
+                        break;
+                    case "Producto 8":
+                        selectedPictureBox = pictureBoxProd8;
+                        break;
+                    default:
+                        MessageBox.Show("No se encontró el PictureBox seleccionado.");   /// otro fallo, cuando seleccionas o escribes algo que no es un producto #
+                        return;                                                          /// creo no salta este error por el comboBox
+                }
+                //      pictureBoxProd1.ImageLocation = abrirImagen.FileName;
+                //        pictureBoxProd1.SizeMode = PictureBoxSizeMode.StretchImage;
+                string filePath = abrirImagen.FileName;
+                selectedPictureBox.ImageLocation = filePath;
+                selectedPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                if (!string.IsNullOrWhiteSpace(textBoxNomProd.Text)) /// esto es para que con ayuda del comboBox poderle darle nombre al 
+                {                                                    /// label que esta arriba de la imagen para cada producto 
+                    if (selectedBox == "Producto 1")                 /// igual no se si sea mejor separarlo 
+                        labelNomProd1.Text = textBoxNomProd.Text;
+                    else if (selectedBox == "Producto 2")
+                        labelNomProd2.Text = textBoxNomProd.Text;
+                    else if (selectedBox == "Producto 3")
+                        labelNomProd3.Text = textBoxNomProd.Text;
+                    else if (selectedBox == "Producto 4")
+                        labelNomProd4.Text = textBoxNomProd.Text;
+                    else if (selectedBox == "Producto 5")
+                        labelNomProd5.Text = textBoxNomProd.Text;
+                    else if (selectedBox == "Producto 6")
+                        labelNomProd6.Text = textBoxNomProd.Text;
+                    else if (selectedBox == "Producto 7")
+                        labelNomProd7.Text = textBoxNomProd.Text;
+                    else if (selectedBox == "Producto 8")
+                        labelNomProd8.Text = textBoxNomProd.Text;
+                }
+            }
+        }
+
+        private void Iniciar()
+        {
+            ticket = new DataTable();
+            ticket.Columns.Add("Producto");
+            ticket.Columns.Add("Categoria");
+            ticket.Columns.Add("Existencia");
+            ticket.Columns.Add("Precio");
+            //ticket.Rows.Add("");          ///Hay que poner numeracion de cuantos productos/conjunto de productos van (?) 
+            dataGridViewTicket.DataSource = ticket;
+
+        }
+
+
+        //private void Guardar()
+        //{
+        //    usuarioModel modelo = new usuarioModel()
+        //    {                                                    ejemplo de otro programa de como guardar los datos
+        //        Nombre = textBoxNombre.Text,                     pero es necesario crear dinamicamente un "ticket" 
+        //        Apellidos = textBoxApellidos.Text,               usando clases agregando carpetas como la practica 
+        //        Edad = int.Parse(textBoxEdad.Text),              del video
+        //        Carrera = textBoxCarrera.Text
+        //    };
+        //    dato.Guardar(modelo);
+
+        //}
+        
+
+        private void labelProdNum1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
